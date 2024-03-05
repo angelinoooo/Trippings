f7909a1b582b8252617527e55fa5137c2af09e75
angelinoooo <148028834+angelinoooo@users.noreply.github.com>
2024-03-05 15:24:34 +0800

    Add files via upload

---
+++ LKZW/index.py
@@ -0,0 +1,10 @@
+a = input("How old are you?")
+x = int(a)
+z = int(b)
+if x < 18:
+    print("You are a minor, you can go")
+else:
+   b = input("Do you like programming course?")
+  
+   if z == "yes":
+       print("pake ko")
